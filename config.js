64a41f428a55fb24450a2f5f7107c9ba7aea9fc7
VuDangKhoa <dangkhoa09102004@gmail.com>
2024-09-13 12:01:59 +0700

    Push all project

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    titleWeb: "Gửi em",
+
+    background: "background.jpg",
+
+    min1: "max4.jpg",
+    min2: "min2.png",
+    min3: "max3.jpg",
+    min4: "max4.jpg",
+    min5: "min5.png",
+    min6: "min6.png",
+
+    max1: "max1.jpg",
+    max2: "max2.jpg",
+    max3: "max3.jpg",
+    max4: "max4.jpg",
+    max5: "max5.png",
+    max6: "max6.png",
+}
